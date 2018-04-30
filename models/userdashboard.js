39cb6c73f17f1f9eca74f0d8f941581892ea6ce6
AndrewTrahan <globalART19@gmail.com>
2018-04-30 17:50:50 -0500

    not sure

--- models/userdashboard.js
+++ models/userdashboard.js
@@ -11,19 +11,17 @@ async function getHistoricalData() {
   try {
     await publicClient.getProductHistoricRates(product, { granularity: granularity }, (err, res, data) => {
       if (err) { throw err }
-      const [hdtime, hdlow, hdhigh, hdopen, hdclose, hdvolume] = async () => {
-        let date = new Date(0)
-        await db.HistoricalData.findOrCreate({
-          where: {
-            time: date.setUTCSeconds(+data[1][0]),
-            low: Math.round(+data[1][1] * 100),
-            high: Math.round(+data[1][2] * 100),
-            open: Math.round(+data[1][3] * 100),
-            close: Math.round(+data[1][4] * 100),
-            volume: Math.round(+data[1][5] * 100)
-          }
-        })
-      }
+      // const [hdtime, hdlow, hdhigh, hdopen, hdclose, hdvolume] = async () => {
+      //   let date = new Date(0)
+      //   await db.HistoricalData.create({
+      //     time: date.setUTCSeconds(+data[1][0]),
+      //     low: Math.round(+data[1][1] * 100),
+      //     high: Math.round(+data[1][2] * 100),
+      //     open: Math.round(+data[1][3] * 100),
+      //     close: Math.round(+data[1][4] * 100),
+      //     volume: Math.round(+data[1][5] * 100)
+      //   })
+      // }
       // [ time, low, high, open, close, volume ],
       // [ 1415398768, 0.32, 4.2, 0.35, 4.2, 12.3 ]
     })
