ae495a6cce00cd00b45ea3a6368aa60ae59ddebc
AndrewTrahan <globalART19@gmail.com>
2018-05-23 17:45:43 -0500

    working on maxReturns

--- server/db/algorithmresults.js
+++ server/db/algorithmresults.js
@@ -19,27 +19,29 @@ AlgorithmResults.calcReturns = async function () {
 AlgorithmResults.calcMaxReturn = async function () {
   const histData = await HistoricalData.findAll({ attributes: ['histTime', 'close'], order: [['histTime', 'ASC']] })
   console.log(histData[0].dataValues.close)
-  let lastSell = histData[0].dataValues.close
-  let prevVal = firstVal
   let curQty = 1
-  let firstDecline = true
-  const maxReturn = histData.reduce((acc, curTick) => {
-    const curVal = curTick.dataValues.close
-    const delta = curVal - prevVal
-    if (delta > 0) {
-      prevVal = curVal
-      firstDecline = true
-      return acc + delta * curQty
+  let curBank = 0
+  let tick = 0
+  while (histData[tick + 1]) {
+    let curVal = histData[tick].dataValues.close
+    let nextVal = histData[tick + 1].dataValues.close
+    //buy condition
+    if (nextVal > curVal && curQty === 0) {
+      curQty = curBank / curVal
+      curBank = 0
+      //set buy marker
     }
-    if (firstDecline) {
-      firstDecline = false
-      curQty = curQty * prevVal / lastSell
-      prevVal = curVal
+    //sell condition
+    if (nextVal < curVal && curQty > 0) {
+      curBank = curVal * curQty
+      curQty = 0
+      //set sell marker
     }
-    return acc
-  }, 0)
-  console.log(maxReturn)
-  return maxReturn
+  }
+  const lastVal = histData[histData.length - 1].dataValues.close
+  if (curQty > 0) curBank = lastVal * curQty
+  console.log(curBank)
+  return curBank
 }
 
 module.exports = { AlgorithmResults }
